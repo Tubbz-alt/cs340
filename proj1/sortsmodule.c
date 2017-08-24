88e4424e914dcbbc804fe796570ee5ab7f897ceb
dane-johnson <dane_johnson@ymail.com>
2017-08-23 22:22:43 -0500

    C/Python interop

--- proj1/sortsmodule.c
+++ proj1/sortsmodule.c
@@ -1,11 +1,15 @@
+#include <stdio.h>
 #include <string.h>
-#include <Python.h>
+
+#include "Python.h"
 
 #define INFINITY "~";
 
 void insertion_sort(char**, int);
 void merge_sort(char**, int, int);
 void merge(char**, int, int, int);
+void pylist_to_array(PyObject*, char**, int);
+void array_to_pylist(char**, int, PyObject*);
 
 void insertion_sort(char* arr[], int n) {
   char* curr;
@@ -54,3 +58,62 @@ void merge(char* arr[], int left, int mid, int right) {
     }
   }
 }
+
+void pylist_to_array(PyObject* list, char** arr, int n) {
+  int i;
+  
+  for (i = 0; i < n; i++) {
+    arr[i] = PyString_AsString(PyList_GetItem(list, i));
+  }
+}
+
+void array_to_pylist(char** arr, int n, PyObject* list) {
+  int i;
+
+  for (i = 0; i < n; i++) {
+    PyList_SetItem(list, i, PyString_FromString(arr[i]));
+  }
+}
+
+static PyObject* py_insertion_sort(PyObject* self, PyObject* args) {
+  char** arr;
+  int n;
+
+  PyObject *list;
+  PyArg_ParseTuple(args, "O", &list);
+
+  n = PyList_Size(list);
+  arr = calloc(n, sizeof(char**));
+  pylist_to_array(list, arr, n);
+  insertion_sort(arr, n);
+  array_to_pylist(arr, n, list);
+  free(arr);
+  return Py_None;
+}
+
+static PyObject* py_merge_sort(PyObject* self, PyObject* args) {
+  char** arr;
+  int n;
+
+  PyObject *list;
+  PyArg_ParseTuple(args, "O", &list);
+
+  n = PyList_Size(list);
+  arr = calloc(n, sizeof(char**));
+  pylist_to_array(list, arr, n);
+  merge_sort(arr, 0, n);
+  array_to_pylist(arr, n, list);
+  free(arr);
+  return Py_None;
+}
+
+static PyMethodDef SortsMethods[] = {
+  {"insertion_sort", py_insertion_sort, METH_VARARGS, ""},
+  {"merge_sort", py_merge_sort, METH_VARARGS, ""},
+  {NULL, NULL, 0, NULL} //Sentinel
+};
+
+PyMODINIT_FUNC initsorts(void) {
+  (void) Py_InitModule("sorts", SortsMethods);
+}
+  
--- proj1/test.py
+++ proj1/test.py
@@ -1,14 +1,4 @@
-from sorts import insertion_sort, merge, merge_sort
-
-arr = [3, 2, 1, 4, 80, 3, 6, 10, 6]
-print arr
-insertion_sort(arr)
-print arr
-
-arr = ['corn', 'carn', 'cow']
-print arr
-merge(arr, 0, 1, 3)
-print arr
+from sorts import insertion_sort, merge_sort
 
 arr = ['a', 'c', 'b', 'f', 'd', 'aa', 'zzzzzz']
 print arr
