775c9cfc2efc99298129117209c25a057da76ee8
dane-johnson <dane_johnson@ymail.com>
2017-08-24 19:16:42 -0500

    Added heap sort

--- proj1/sortsmodule.c
+++ proj1/sortsmodule.c
@@ -3,11 +3,18 @@
 
 #include "Python.h"
 
-#define INFINITY "~";
+#define INFINITY "~"
+#define LEFT(i) (i*2+1)
+#define RIGHT(i) (i*2+2)
+#define PARENT(i) (i-1/2)
 
 void insertion_sort(char**, int);
 void merge_sort(char**, int, int);
 void merge(char**, int, int, int);
+void heapsort(char**);
+void heapify(char**, int, int);
+void build_heap(char**, int);
+void swap(char**, int, int);
 void pylist_to_array(PyObject*, char**, int);
 void array_to_pylist(char**, int, PyObject*);
 
@@ -59,6 +66,49 @@ void merge(char* arr[], int left, int mid, int right) {
   }
 }
 
+void heap_sort(char** arr, int n){
+  int i;
+  
+  build_heap(arr, n);
+  for (i = n - 1; i < 1; i--) {
+    swap(arr, 0, i);
+    n--;
+    heapify(arr, n, 0);
+  }
+}
+
+void build_heap(char** arr, int n) {
+  int i;
+  for (i = (n - 1) / 2; i >= 0; i--){
+    heapify(arr, n, i);
+  }
+}
+
+void heapify(char** arr, int n, int i) {
+  int l, r, pos;
+  
+  l = LEFT(n);
+  r = RIGHT(n);
+  pos = i;
+  if (l < n && strcmp(arr[l], arr[i]) < 0) {
+    pos = l;
+  }
+  if (r < n && strcmp(arr[r], arr[pos]) < 0) {
+    pos = r;
+  }
+  if (pos != i) {
+    swap(arr, i, pos);
+    heapify(arr, n, pos);
+  }
+}
+
+void swap(char** arr, int i, int j) {
+  char* temp;
+  temp = arr[j];
+  arr[j] = arr[i];
+  arr[i] = temp;
+}
+
 void pylist_to_array(PyObject* list, char** arr, int n) {
   int i;
   
@@ -107,9 +157,43 @@ static PyObject* py_merge_sort(PyObject* self, PyObject* args) {
   return Py_None;
 }
 
+static PyObject* py_heap_sort(PyObject* self, PyObject* args) {
+  char** arr;
+  int n;
+
+  PyObject *list;
+  PyArg_ParseTuple(args, "O", &list);
+
+  n = PyList_Size(list);
+  arr = calloc(n, sizeof(char**));
+  pylist_to_array(list, arr, n);
+  heap_sort(arr, n);
+  array_to_pylist(arr, n, list);
+  free(arr);
+  return Py_None;
+}
+
+static PyObject* py_build_heap(PyObject* self, PyObject* args){
+  char** arr;
+  int n;
+
+  PyObject *list;
+  PyArg_ParseTuple(args, "O", &list);
+
+  n = PyList_Size(list);
+  arr = calloc(n, sizeof(char**));
+  pylist_to_array(list, arr, n);
+  build_heap(arr, n);
+  array_to_pylist(arr, n, list);
+  free(arr);
+  return Py_None;
+}
+
 static PyMethodDef SortsMethods[] = {
   {"insertion_sort", py_insertion_sort, METH_VARARGS, ""},
   {"merge_sort", py_merge_sort, METH_VARARGS, ""},
+  {"heap_sort", py_heap_sort, METH_VARARGS, ""},
+  {"build_heap", py_build_heap, METH_VARARGS, ""},
   {NULL, NULL, 0, NULL} //Sentinel
 };
 
