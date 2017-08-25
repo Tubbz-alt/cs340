f2f73cd8442c660a8e1ceab28ea8ea82d43d1dd2
dane-johnson <dane_johnson@ymail.com>
2017-08-24 19:20:48 -0500

    Updated python test

--- proj1/test.py
+++ proj1/test.py
@@ -1,4 +1,5 @@
-from sorts import insertion_sort, merge_sort
+#!/usr/bin/env python
+from sorts import insertion_sort, merge_sort, heap_sort, build_heap
 
 arr = ['a', 'c', 'b', 'f', 'd', 'aa', 'zzzzzz']
 print arr
@@ -8,4 +9,14 @@ print arr
 arr = ['a', 'c', 'b', 'f', 'd', 'aa', 'zzzzzz']
 print arr
 insertion_sort(arr)
+print arr
+
+arr = ['a', 'c', 'b', 'f', 'd', 'aa', 'zzzzzz']
+print arr
+heap_sort(arr)
+print arr
+
+arr = ['a', 'c', 'b', 'f', 'd', 'aa', 'zzzzzz']
+print arr
+build_heap(arr)
 print arr
