1f0d6c4414cf4c6d72a9de2bf8e076f5389ccfb2
dane-johnson <dane_johnson@ymail.com>
2017-10-19 23:40:33 -0500

    Added parent dict to help find shortest path

--- proj5/sp.py
+++ proj5/sp.py
@@ -6,36 +6,39 @@ INFINITY = sys.maxint
 def init_single_source(graph, source):
   d = {v: INFINITY for v in graph}
   d[source] = 0
-  return d
+  parent = {v: v for v in graph}
+  return d, parent
 
-def relax(graph, d, v, adj, weight):
+def relax(graph, d, parent, v, adj, weight):
   curr = d[v] + weight
-  d[adj] = min([curr, d[adj]])
+  if curr < d[adj]:
+    d[adj] = curr
+    parent[adj] = v
 
 def dag_sp(graph, vec, source):
-  d = init_single_source(graph, source)
+  d, parent = init_single_source(graph, source)
   for v in vec:
     for adj, weight in graph[v]:
-      relax(graph, d, v, adj, weight)
-  return d
+      relax(graph, d, parent, v, adj, weight)
+  return d, parent
 
 def bellman_ford(graph, source):
-  d = init_single_source(graph, source)
+  d, parent = init_single_source(graph, source)
   for i in xrange(1, len(graph)):
     for v in graph:
       for adj, weight in graph[v]:
-        relax(graph, d, v, adj, weight)
+        relax(graph, d, parent, v, adj, weight)
   for v in graph:
     for adj, weight in graph[v]:
       if d[adj] > d[v] + weight:
         return None
-  return d
+  return d, parent
 
 def dijkstra(graph, source):
   def compare(x, y): return x[1] - y[1]
   def key(x): return x[0]
   def update(x, y): x[1] = y
-  d = init_single_source(graph, source)
+  d, parent = init_single_source(graph, source)
   pq  = PriorityQueue(compare, key, update)
   for v in graph:
     pq.insert([v, INFINITY])
@@ -47,6 +50,7 @@ def dijkstra(graph, source):
       elem = pq.get(adj)
       if elem and elem[1] > d[v] + weight:
         pq.decrease_priority(adj, d[v] + weight)
-  return d
+        parent[adj] = v
+  return d, parent
         
   
